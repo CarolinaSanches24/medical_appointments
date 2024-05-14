72f153e8c9a655c224e5590a31993c0dbf425457
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-05-14 18:11:31 -0300

    feat: add config drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,12 +4,12 @@ import { env } from './api/src/utils/dotenv/env'
 export const dbUrl = `mysql://${env.variables.DB_USER}:${env.variables.DB_PASSWORD}@${env.variables.DB_HOST}:${env.variables.DB_PORT}/${env.variables.DB_NAME}`
 
 export default {
-  schema: "./api/src/services/schemas/user/userShema.ts*",
+  schema: "./api/src/services/schemas/user/userShema.ts",
   out: "./drizzle",
-  driver: 'mysql2', 
-  dbCredentials: {
-    connectionString: String(dbUrl),
-  },
-  verbose:true,
-  strict:true,
-} as Config;
+  // driver: 'mysql2', 
+  // dbCredentials: {
+  //   connectionString: String(dbUrl),
+  // },
+  // verbose:true,
+  // strict:true,
+} satisfies Config;
